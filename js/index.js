42e7b3aeecb8ebf984c1d3c9ee0e1dd16d2aa049
Daniel Meeusen <danmeeusen@gmail.com>
2018-03-25 19:16:52 -0700

    everything

--- js/index.js
+++ js/index.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
 	$('.searchresults').hide();
 	
 	$('.topSearch-btn').on("click", function(){
-		initToTop();
 		topSearchResults();
 	});
 	
@@ -11,15 +10,30 @@ $(document).ready(function(){
 		initToTop();
 		searchResults();
 	});
+	
+	var top = false;
 
-	var topOrBottom = searchResults();
 	// Submits form to reformat page and display search result.
 	// not sure why 'event.preventDefault did not work here vs 'return false.' if someone can explain please comment.
 	$('form').submit(function(event){
-		initToTop();
-		searchResults();
+		if (top === true) {
+			topSearchResults();
+		} else {
+			searchResults();
+			initToTop();
+		}
 		return false;
 	});	
+	
+	
+	
+	function placeFooter() {
+		if ($("body").height() > $(document).height()) {
+			$('footer').css('position', 'relative');
+    } else {
+			$('footer').css('position', 'absolute');
+		}
+	}
 
 				$("#topSearch").autocomplete({
 					delay: 0,
@@ -67,8 +81,11 @@ $(document).ready(function(){
 			}
 		});
 	
+	
+	
 	function topSearchResults() {
 		var topSearchTerm = $('#topSearch').val();
+		console.log(topSearchTerm);
 		$.ajax({
 			url: 'https://en.wikipedia.org/w/api.php',
 			dataType: "jsonp",
@@ -82,17 +99,19 @@ $(document).ready(function(){
 						var link = data[3];
 						var description = data[2];
 						var resultCards = [];
-						console.log(data);					
 						for (var i = 0; i < title.length; i++) {
 							resultCards.push(`<div class="resultCard"><a href="${link[i]}" target="_blank"> ${title[i]}</></a><p>${description[i]}</p></div>`);
 						}
 						$('.searchResults').html(resultCards);
 					}					
 		});
+		$('#topSearch').blur();
+		$('#topSearch').val('');
 	}
-	
+
 	function searchResults() {
-	var searchTerm = $('#wikisearch').val();
+		var searchTerm = $('#wikisearch').val();
+		console.log(searchTerm);
 		$.ajax({
 			url: 'https://en.wikipedia.org/w/api.php',
 			dataType: "jsonp",
@@ -106,22 +125,22 @@ $(document).ready(function(){
 						var link = data[3];
 						var description = data[2];
 						var resultCards = [];
-						console.log(data);				
 						for (var i = 0; i < title.length; i++) {
 							resultCards.push(`<div class="resultCard"><a href="${link[i]}" target="_blank"> ${title[i]}</></a><p>${description[i]}</p></div>`);
 						}
 						$('.searchResults').html(resultCards);
+						placeFooter();	
 					}					
 		});
 	}
 								
 	function initToTop() {
-		  topOrBottom = topSearchResults();
-			$(".input-group").hide();
-			$(".topNav").show();
-			$(".randomlink").hide();
-			$('.searchresults').show();
-			$('body').css("background-image", "linear-gradient( rgba(0, 0, 0, .85), rgba(0, 0, 0, .25) ),url('https://s19.postimg.org/ubtvuuavn/stacks.jpg')");
+		top = true;
+		$(".input-group").hide();
+		$(".topNav").show();
+		$(".randomlink").hide();
+		$('.searchresults').show();
+		$('body').css("background-image", "linear-gradient( rgba(0, 0, 0, .85), rgba(0, 0, 0, .25) ),url('https://s19.postimg.org/ubtvuuavn/stacks.jpg')");
 	}
 	
 	// Darkens background when searchbox is in focus.
@@ -133,5 +152,5 @@ $(document).ready(function(){
 	$("#wikisearch").blur(function(){
 		$('body').css("background-image", "linear-gradient( rgba(0, 0, 0, .65), rgba(0, 0, 0, .0) ),url('https://s19.postimg.org/ubtvuuavn/stacks.jpg')");
 	});
-	
+
 });
